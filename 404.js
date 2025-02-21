726581f3a484ca34fbc8218f12e3497187d8cab0
Eeviika <84326942+Eeviika@users.noreply.github.com>
2025-02-21 14:15:44 -0500

    Add random error messages to 404 page because funny

---
+++ 404.js
@@ -0,0 +1,19 @@
+const messages = [
+    `The page you are looking for might have been removed, or had its name changed.`,
+    `Sorry, but whatever you're looking for isn't here.`,
+    `Insert error message here.`,
+    `The page you are looking for is not available.`,
+    `"But you're not supposed to be here, Mr. Freeman..."`,
+    `"I'm sorry, Dave. I'm afraid I can't do that."`,
+    `"Thank you, Mario! But our page is in another castle!"`,
+    `"All your base are belong to us."`,
+    `"Snake? Snake?! SNAAAAKE!"`,
+    `"Cortana? I think we're lost."`,
+    `"The cake is a lie."`,
+    `"It's dangerous to go alone! Take this 404 error!"`
+]
+
+const message = messages[Math.floor(Math.random() * messages.length)];
+
+document.getElementById("errorMessage").textContent = message;
+document.getElementById("errorDetails").textContent = `You attempted to access ${window.location.href} at ${new Date().toLocaleString()}.`;
